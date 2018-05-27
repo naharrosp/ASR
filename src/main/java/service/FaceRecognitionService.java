790da45f65b3549ef782fe7656c76aeeee81de5d
fandecaICAI <201307390@alu.comillas.edu>
2018-05-27 23:25:25 +0200

    face recognition init

---
+++ src/main/java/service/FaceRecognitionService.java
@@ -0,0 +1,35 @@
+package src.main.java.service;
+
+public class FaceRecognitionService {
+
+		  public void init(){
+					 VisualRecognition service = new VisualRecognition("{version}");
+					 // Set the endpoint
+					 service.setEndPoint("https://gateway.watsonplatform.net/visual-recognition/api");
+					 IamOptions options = new IamOptions.Builder()
+								.url("") // Optional
+								.apiKey("{iam_api_key}")
+								.build();
+					 service.setIamCredentials(options);
+
+		  }
+
+
+		  public void processImage(){
+					 VisualRecognition service = new VisualRecognition("2018-03-19");
+					 service.setEndPoint("https://gateway.watsonplatform.net/visual-recognition/api");
+
+					 IamOptions options = new IamOptions.Builder().apiKey("{iam_api_key}")
+								service.setIamCredentials(options);
+
+					 DetectFacesOptions detectFacesOptions = new DetectFacesOptions.Builder()
+								.imagesFile(new File("./Ginni_Rometty.jpg"))
+								.build();
+					 DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
+					 System.out.println(result);
+		  }
+
+
+
+
+}
